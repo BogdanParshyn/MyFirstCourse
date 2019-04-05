19fb97461cd4387f5ade58e6b6404d2818e6b702
Module <bogdan.alexsandrovich@gmail.com>
2019-04-05 19:24:50 +0300

    18

---
+++ Lesson18/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log('b' > 'a');
+
+console.log('Банан' > 'Анна');
+
+console.log('2' > 1);
+
+console.log('0' == false); // true
+
+console.log('' == false); // true
+
+console.log('0' === false); // false
+
+console.log('0' !== false); // true
